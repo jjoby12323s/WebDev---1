086c624c44ebda47d26ad788df5352177c964b5b
jjoby12323s <jonathanmathew.joby2020@vitstudent.ac.in>
2021-02-01 18:52:35 +0100

    Final Code Done

---
+++ index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const squares = 2500;
+
+for(let i=0;i<squares;i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.append(square);
+    console.log(square)
+    square.addEventListener('mouseover',function(){
+        const color = colors[Math   .floor(Math.random()*colors.length)];
+        square.style.backgroundColor = color;
+        square.style.boxShadow = `0 0 2px ${color} , 0 0 10px ${color}`;
+    });
+    square.addEventListener('mouseout',function(){
+        square.style.backgroundColor = '#1e1e1e';
+        square.style.boxShadow = `0 0 2px #000`;
+    });
+    container.append(square);
+}
